d42074b3dfc549f75381a1da33e391729b499108
mzc02a <zakariachemam.site@gmail.com>
2026-04-20 23:29:02 +0200

    Exercice 7 - ajout variable machines avec validation

--- variables.tf
+++ variables.tf
@@ -1,29 +1,49 @@
-variable "image_name" {
-  description = "Nom de l'image Docker"
-  type        = string
-  default     = "nginx:latest"
-}
+variable "machines" {
+  description = "Liste des machines virtuelles à déployer"
 
-variable "container_name" {
-  description = "Nom du conteneur"
-  type        = string
-  default     = "nginx-terraform"
-}
+  type = list(object({
+    name      = string
+    vcpu      = number
+    disk_size = number
+    region    = string
+  }))
 
-variable "external_port" {
-  description = "Port externe"
-  type        = number
-  default     = 8080
-}
+  default = [
+    {
+      name      = "vm-app"
+      vcpu      = 2
+      disk_size = 20
+      region    = "eu-west-1"
+    },
+    {
+      name      = "vm-db"
+      vcpu      = 4
+      disk_size = 50
+      region    = "us-east-1"
+    }
+  ]
 
-variable "internal_port" {
-  description = "Port interne"
-  type        = number
-  default     = 80
-}
+  validation {
+    condition = alltrue([
+      for machine in var.machines :
+      machine.vcpu >= 2 && machine.vcpu <= 64
+    ])
+    error_message = "Chaque machine doit avoir un nombre de vCPU entre 2 et 64."
+  }
 
-variable "server_names" {
-  description = "Liste des noms des serveurs clients"
-  type        = list(string)
-  default     = ["app", "api", "db"]
+  validation {
+    condition = alltrue([
+      for machine in var.machines :
+      machine.disk_size >= 20
+    ])
+    error_message = "Chaque machine doit avoir une taille de disque supérieure ou égale à 20 Go."
+  }
+
+  validation {
+    condition = alltrue([
+      for machine in var.machines :
+      contains(["eu-west-1", "us-east-1", "ap-southeast-1"], machine.region)
+    ])
+    error_message = "La région doit être l'une des suivantes : eu-west-1, us-east-1, ap-southeast-1."
+  }
 }
