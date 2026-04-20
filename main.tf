9125b858c4feb0e024f1f0c0dfdf94c7758c6177
mzc02a <zakariachemam.site@gmail.com>
2026-04-20 23:18:21 +0200

    Exercice 4 - ajout reseau docker et conteneur client

--- main.tf
+++ main.tf
@@ -11,18 +11,28 @@ terraform {
   }
 }
 
-# Provider Docker (Windows)
 provider "docker" {
   host = "tcp://localhost:2375"
 }
 
-# Image Docker
+# Réseau Docker dédié
+resource "docker_network" "nginx_network" {
+  name = "nginx-network"
+}
+
+# Image Nginx
 resource "docker_image" "nginx" {
   name         = var.image_name
   keep_locally = true
 }
 
-# Conteneur Docker
+# Image curl
+resource "docker_image" "curl" {
+  name         = "appropriate/curl"
+  keep_locally = true
+}
+
+# Conteneur Nginx
 resource "docker_container" "nginx" {
   name  = var.container_name
   image = docker_image.nginx.image_id
@@ -31,13 +41,36 @@ resource "docker_container" "nginx" {
     internal = var.internal_port
     external = var.external_port
   }
+
+  networks_advanced {
+    name    = docker_network.nginx_network.name
+    aliases = ["nginx"]
+  }
 }
 
-# Test automatique Nginx
+# Test automatique depuis la machine locale
 resource "null_resource" "test_nginx" {
   depends_on = [docker_container.nginx]
 
   provisioner "local-exec" {
     command = "curl http://localhost:${var.external_port} | findstr Welcome"
+  }
+}
+
+# Conteneur client
+resource "docker_container" "client" {
+  name  = "client"
+  image = docker_image.curl.image_id
+
+  depends_on = [docker_container.nginx]
+
+  command = [
+    "sh",
+    "-c",
+    "curl http://nginx:80 && sleep 30"
+  ]
+
+  networks_advanced {
+    name = docker_network.nginx_network.name
   }
 }
