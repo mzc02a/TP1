d7ebd01a125cd9a378f0d44cbd7a760f7b4373ec
mzc02a <zakariachemam.site@gmail.com>
2026-04-20 23:12:47 +0200

    Correction main.tf avec provider null + test nginx

--- main.tf
+++ main.tf
@@ -4,16 +4,22 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
   }
 }
 
+# Provider Docker (Windows)
 provider "docker" {
   host = "tcp://localhost:2375"
 }
 
 # Image Docker
 resource "docker_image" "nginx" {
-  name = var.image_name
+  name         = var.image_name
+  keep_locally = true
 }
 
 # Conteneur Docker
@@ -24,5 +30,14 @@ resource "docker_container" "nginx" {
   ports {
     internal = var.internal_port
     external = var.external_port
+  }
+}
+
+# Test automatique Nginx
+resource "null_resource" "test_nginx" {
+  depends_on = [docker_container.nginx]
+
+  provisioner "local-exec" {
+    command = "curl http://localhost:${var.external_port} | findstr Welcome"
   }
 }
